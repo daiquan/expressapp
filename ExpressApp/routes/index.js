47cd3e078004d5fe0a2ac99a266094ce20dc0a75
daiquan <daiquan1121@hotmail.com>
2016-03-02 21:08:37 -0600

    add new page

--- ExpressApp/routes/index.js
+++ ExpressApp/routes/index.js
@@ -14,3 +14,7 @@ exports.about = function (req, res) {
 exports.contact = function (req, res) {
     res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
 };
+
+exports.new = function (req, res) {
+    res.render('new', { title: 'New', year: new Date().getFullYear(), message: 'Your contact page' });
+};
